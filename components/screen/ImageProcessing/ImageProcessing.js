8dfdf07a01290c4146a8f1240a49e20426fa705d
Mudoker <huuquoc7603@gmail.com>
2023-06-19 17:19:29 +0700

    image_process

--- components/screen/ImageProcessing/ImageProcessing.js
+++ components/screen/ImageProcessing/ImageProcessing.js
@@ -1,9 +1,21 @@
 import React, { useState } from 'react';
-import { View, Button, Image } from 'react-native';
-// import ImagePicker from 'react-native-image-picker';
+import { styles } from './ImageProcess.style';
+import { View, Button, Image, Touchable } from 'react-native';
+import {
+  Text,
+  TextInput,
+  SafeAreaView,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  StatusBar,
+  TouchableWithoutFeedback,
+} from 'react-native';
+
 var ImagePicker = require('react-native-image-picker');
 const PhotoSelectionPage = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImages, setSelectedImages] = useState([]);
+  const MAX_IMAGES = 5;
 
   const handleChoosePhoto = () => {
     const options = {
@@ -12,6 +24,8 @@ const PhotoSelectionPage = () => {
       maxWidth: 500,
       maxHeight: 500,
       quality: 1,
+      multiple: true,
+      maxFiles: MAX_IMAGES - selectedImages.length,
     };
 
     ImagePicker.launchImageLibrary(options, (response) => {
@@ -22,7 +36,16 @@ const PhotoSelectionPage = () => {
       } else if (response.customButton) {
         console.log('User tapped custom button: ', response.customButton);
       } else {
-        setSelectedImage(response.uri);
+        const newImages = response.assets.map((asset) => ({
+          uri: asset.uri,
+          name: asset.fileName || `image_${Date.now()}`,
+        }));
+        setSelectedImages((prevImages) => [...prevImages, ...newImages]);
+        newImages.forEach((image) => {
+          callAPI(image, () => {
+            deleteImage(image);
+          });
+        });
       }
     });
   };
@@ -42,22 +65,93 @@ const PhotoSelectionPage = () => {
       } else if (response.error) {
         console.log('Camera Error: ', response.error);
       } else {
-        setSelectedImage(response.uri);
+        const newImage = {
+          uri: response.uri,
+          name: `image_${Date.now()}`,
+        };
+        setSelectedImages((prevImages) => [...prevImages, newImage]);
+        callAPI(newImage, () => {
+          deleteImage(newImage);
+        });
       }
     });
   };
 
+  const callAPI = async (image, callback) => {
+    try {
+      const formData = new FormData();
+      formData.append('image', {
+        uri: image.uri,
+        type: 'image/jpeg',
+        name: image.name,
+      });
+
+      const response = await fetch('http://1.52.246.101:5000/handle-lcd/handle-lcd', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const data = await response.json();
+      console.log('API Response:', data);
+
+      // Invoke the callback function after API call is completed
+      if (typeof callback === 'function') {
+        callback();
+      }
+    } catch (error) {
+      console.log('API Error:', error);
+    }
+  };
+
+  const deleteImage = (imageToDelete) => {
+    setSelectedImages((prevImages) =>
+      prevImages.filter((image) => image.uri !== imageToDelete.uri)
+    );
+  };
+
   return (
     <View>
-      <Button title="Choose Photo from Library" onPress={handleChoosePhoto} />
-      <Button title="Take a Photo" onPress={handleTakePhoto} />
+      <View style={{backgroundColor: '#6C56F5', height:40}}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image style={[styles.backIcon, {marginTop:5}]} source={require('../../../assets/images/nav.png')}></Image>
+          <Image style={[styles.backIcon, {marginTop:10,marginLeft: 350, width:22, height:22}]} source={require('../../../assets/images/setting_icon.png')}></Image>
+          <TouchableOpacity style={[styles.backIcon, {backgroundColor:'#F53030', borderRadius: 11,marginTop:10,marginLeft: 390, width:22, height:22}]} onPress={() => navigation.goBack()}> 
+            <Text style={{color:'white', width:11 ,position:'absolute', marginLeft: 5.5}}>V</Text>
+          </TouchableOpacity>
+        </TouchableOpacity>
+        <Image style={[styles.header, {marginTop:5}]} source={require('../../../assets/images/INAS_mobile_logo_2.png')}></Image>
+      </View>
+      <Text style={styles.title}>Đọc máy đo</Text>
+      <Text style={styles.footNote}>Bạn vui lòng chọn một trong hai để sử dụng hiệu quả.</Text>
+      <TouchableOpacity
+        style={[styles.buttonLibrary, {top:350} ,selectedImages.length >= MAX_IMAGES && styles.disabledButton]}
+        onPress={handleChoosePhoto}
+        disabled={selectedImages.length >= MAX_IMAGES}
+      >
+        <Image style={styles.buttonImage} source={require('../../../assets/images/image_lib.png')}></Image>
+        <Text style={styles.buttonText}>
+          Drag-n-Drop to upload
+        </Text>
+      </TouchableOpacity>
 
-      {selectedImage && (
+      <TouchableOpacity
+        style={[styles.buttonLibrary, {top: 500}]}
+        onPress={handleTakePhoto}
+      >
+        <Image style={styles.buttonImage} source={require('../../../assets/images/image_photo.png')}></Image>
+        <Text style={styles.buttonText}>
+          Take a photo
+        </Text>
+      </TouchableOpacity>
+
+      {/* Show selected images
+      {selectedImages.map((image) => (
         <Image
-          source={{ uri: selectedImage }}
+          key={image.uri}
+          source={{ uri: image.uri }}
           style={{ width: 200, height: 200 }}
         />
-      )}
+      ))} */}
     </View>
   );
 };
